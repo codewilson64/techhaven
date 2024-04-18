2a8b0222898b11141562d9dddf22d4fdd5548e87
codewilson64 <wilsongambit@gmail.com>
2024-04-18 15:13:54 +0700

    change api url

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export async function productsData() {
-  const products = await axios.get("http://localhost:8000/products");
+  const products = await axios.get("https://codewilson64.github.io/apple-product-api/db.json");
   return products;
 }
